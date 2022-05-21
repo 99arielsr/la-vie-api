c7e7a1f84e25dcba931043b335052daaad341207
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-05-21 18:47:04 -0300

    Delete node_modules directory

--- node_modules/express-validation/lib/joi.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-const { parameters } = require('./parameters');
-
-const schema = parameters
-  .reduce((result, item) => ({ ...result, [item]: Joi.object().schema() }), {});
-
-const joiSchema = Joi.object(schema).required().min(1);
-
-exports.joiSchema = joiSchema;
--- node_modules/express-validation/lib/mutation.js
+++
@@ -1,7 +0,0 @@
-exports.handleMutation = (request, parameter, value, mutate) => {
-  if (mutate) {
-    request[parameter] = value;
-  }
-
-  return null;
-};
