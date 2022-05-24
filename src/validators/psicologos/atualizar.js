2114eb675e384b986879be9460714d9209eac868
Adriano Leal <adrianoleal35@gmail.com>
2022-05-24 15:30:29 -0300

    Verif de retorno da validação da senha ref .min(6)

---
+++ src/validators/psicologos/atualizar.js
@@ -0,0 +1,10 @@
+const { validate, Joi } = require("express-validation");
+
+module.exports = validate({
+  body: Joi.object({
+    nome: Joi.string(),
+    email: Joi.string().email(),
+    apresentacao: Joi.string(),
+    senha: Joi.string().min(6),
+  }),
+});
